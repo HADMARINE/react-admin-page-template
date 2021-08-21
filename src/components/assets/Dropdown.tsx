3f35663719e832410a9ee7d09231c73e1ca5ed7d
hadmarine <tsx@kakao.com>
2021-08-22 08:38:31 +0900

    Fix function name

--- src/components/assets/Dropdown.tsx
+++ src/components/assets/Dropdown.tsx
@@ -111,7 +111,7 @@ const Dropdown = (props: Props) => {
           <DropdownMenuWrapper>
             {props.choices?.map((value) => {
               return (
-                <DropdownItem key={Assets.getRandomNumber(10000, 99999)}>
+                <DropdownItem key={Assets.arbitrary(10000, 99999)}>
                   {value}
                 </DropdownItem>
               );
