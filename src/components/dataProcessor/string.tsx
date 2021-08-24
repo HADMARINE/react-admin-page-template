3bbed73a771cb96805eb7f9325bd4eb3e0ccf667
HADMARINE <tsx@kakao.com>
2021-08-24 14:32:44 +0900

    Update string container

--- src/components/dataProcessor/string.tsx
+++ src/components/dataProcessor/string.tsx
@@ -9,7 +9,19 @@ interface Props extends ContainerBase<string> {
   center?: boolean;
   left?: boolean;
   right?: boolean;
+  fontSize?: string;
+  fontWeight?: number;
 }
+
+const justifyDirection = (
+  props: Props,
+  action: { center: string; left: string; right: string },
+) => {
+  if (props.left) return action.left;
+  else if (props.right) return action.right;
+  else if (props.center) return action.center;
+  return '';
+};
 
 const StringContainer = (props: Props) => {
   return (
@@ -19,12 +31,31 @@ const StringContainer = (props: Props) => {
       center={props.center}
       left={props.left}
       right={props.right}
-      onClick={props.onClick}>
-      {props.isChangeState ? (
-        <Input value={props.value} onChange={props.onChange} />
+      onClick={props.onClick}
+      style={{ width: '100%', height: '100%', margin: '2px' }}>
+      {props.isChanging ? (
+        <Input
+          value={props.value}
+          onChange={props.onChange}
+          name={props.name}
+          variant={'primary'}
+          fontSize={props.fontSize}
+          fontWeight={props.fontWeight}
+        />
       ) : (
         <Color.key>
-          <Text>{props.value}</Text>
+          <Text
+            width={'100%'}
+            height={'100%'}
+            fontSize={props.fontSize}
+            fontWeight={props.fontWeight}
+            textAlign={justifyDirection(props, {
+              center: 'center',
+              left: 'left',
+              right: 'right',
+            })}>
+            {props.value}
+          </Text>
         </Color.key>
       )}
     </Flex>
