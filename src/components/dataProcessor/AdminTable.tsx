113672024eca98d68aee1a68a7cc0e4e8b82b54e
HADMARINE <tsx@kakao.com>
2021-08-25 09:38:22 +0900

    WIP : Update admin table

--- src/components/dataProcessor/AdminTable.tsx
+++ src/components/dataProcessor/AdminTable.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AdminTableApi, ContainerBase, ExclusiveContainerBase } from '.';
 import Color from '../assets/Color';
 import { Flex } from '../assets/Wrapper';
+import colorSettings from '@settings/color.json';
 
 interface Props<T extends Record<string, ContainerBase<any>>> {
   contents: T;
@@ -34,18 +35,68 @@ const AdminTable = function <T extends Record<string, any>>(props: Props<T>) {
   return (
     <Flex vertical>
       <Color.key>
+        <Flex horizontal>
+          {Object.entries(props.contents).map(([k, v], _idx) => {
+            return (
+              <Flex
+                style={{
+                  border: '1px solid',
+                  borderRadius: '5px',
+                  fontSize: '24px',
+                  margin: '1px',
+                  backgroundColor: colorSettings.keyColor,
+                  color: colorSettings.backgroundColor,
+                }}
+                key={`AdminTable_title_iter_${_idx}`}
+                flex={v.pref.flex || 1}>
+                <Flex style={{ marginLeft: '5px' }}>
+                  {
+                    (
+                      v as {
+                        func: (
+                          __props: ExclusiveContainerBase<any>,
+                        ) => JSX.Element;
+                        pref: Omit<
+                          ContainerBase<any>,
+                          keyof ExclusiveContainerBase<any>
+                        >;
+                      }
+                    ).pref.title
+                  }
+                </Flex>
+              </Flex>
+            );
+          })}
+        </Flex>
         {data.data?.map((d: any, idx: number) => {
           return (
-            <Flex horizontal key={`AdminTable_${idx}`}>
-              {Object.entries(props.contents).map(([k, v]) => {
+            <Flex
+              horizontal
+              key={`AdminTable_${idx}`}
+              style={{ border: '1px solid', marginTop: '-1px' }}>
+              {Object.entries(props.contents).map(([k, v], __idx) => {
                 return (
-                  // eslint-disable-next-line no-unused-vars
-                  (v as (__props: ExclusiveContainerBase<any>) => JSX.Element)({
-                    value: d[k],
-                    isChanging: false,
-                    name: k,
-                    onChange: () => 0,
-                  })
+                  <Flex
+                    flex={v.pref.flex || 1}
+                    style={{ borderRight: '1px solid', marginRight: '-1px' }}
+                    key={`AdminPage__idx_${__idx}`}>
+                    {(
+                      v as {
+                        func: (
+                          __props: ExclusiveContainerBase<any>,
+                        ) => JSX.Element;
+                        pref: Omit<
+                          ContainerBase<any>,
+                          keyof ExclusiveContainerBase<any>
+                        >;
+                      }
+                    ).func({
+                      value: d[k],
+                      isChanging: false,
+                      name: k,
+                      onChange: () => 0, // todo this
+                    })}
+                  </Flex>
                 );
               })}
             </Flex>
